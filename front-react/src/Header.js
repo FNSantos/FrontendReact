2019ba57255086913698e7b6a6d4f4505bf3b2a0
Felipe Nascimento <felipenascimento011@outlook.com>
2022-08-25 01:06:17 -0300

    Header

---
+++ front-react/src/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+        <h1>developers</h1>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Sobre</a>
+            <a href="#">Serviços</a>
+            <a href="#">Contato</a>
+        </nav>
+    </header>
+  );
+}
+
+export default Header;
